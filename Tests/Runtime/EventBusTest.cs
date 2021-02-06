d04eacd5c54f79a12467bceec80d2f02f9297059
Slantar <albertouma92@gmail.com>
2021-02-06 17:48:29 +0100

    Add EventBus tests souce code file  - Remove samples

---
+++ Tests/Runtime/EventBusTest.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+
+namespace Slantar.Architecture.Tests
+{
+	public class EventBusTest 
+	{
+		[Test]
+		private void Subscribe()
+		{
+
+		}	
+
+		[Test]
+		private void Trigger()
+		{
+
+		}
+
+		[Test]
+		private void Remove()
+		{
+
+		}
+
+		[Test]
+		private void TriggerEventToDestrolledClass()
+		{
+
+		}
+
+		[Test]
+		private void ClearAllEvents()
+		{
+			
+		}
+	}
+}
